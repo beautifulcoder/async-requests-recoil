33d20ad629fb779266af54b151b36810e641dc4d
Camilo Reyes <CReyes@paylocity.com>
2021-08-05 07:41:26 -0500

    Sample code for asynchronous requests with recoil article

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react'
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  selectorFamily,
+  useRecoilValue,
+  //useSetRecoilState,
+  useRecoilCallback
+} from 'recoil'
 
-function App() {
+const currentWhaleTypesQuery = selector({
+  key: 'CurrentWhaleTypesQuery',
+  get: async () => {
+    const response = await fetch('/whaleTypes')
+    return await response.json()
+  }
+})
+
+const currentWhaleIdState = atom({
+  key: 'CurrentWhaleIdState',
+  default: ''
+})
+
+const whaleInfoQuery = selectorFamily({
+  key: 'WhaleInfoQuery',
+  get: whaleId => async () => {
+    if (whaleId === '') return undefined
+
+    const response = await fetch('/whales/' + whaleId)
+    return await response.json()
+  }
+})
+
+const currentWhaleQuery = selector({
+  key: 'CurrentWhaleQuery',
+  get: ({get}) =>
+    get(whaleInfoQuery(get(currentWhaleIdState)))
+})
+
+function CurrentWhaleTypes() {
+  const whaleTypes = useRecoilValue(currentWhaleTypesQuery)
+  //const setWhaleId = useSetRecoilState(currentWhaleIdState)
+
+  const changeWhale = useRecoilCallback(
+    ({snapshot, set}) => whaleId => {
+      snapshot.getLoadable(whaleInfoQuery(whaleId))
+      set(currentWhaleIdState, whaleId)
+    }
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {whaleTypes.map(whale =>
+        <li key={whale.id}>
+          <a
+            href={"#" + whale.id}
+            onClick={(e) => {
+              e.preventDefault()
+              //setWhaleId(whale.id)
+              changeWhale(whale.id)
+            }}
+          >
+            {whale.name}
+          </a>
+        </li>
+      )}
+    </ul>
+  )
 }
 
-export default App;
+function CurrentWhalePick() {
+  const whale = useRecoilValue(currentWhaleQuery)
+
+  return (
+    <>
+      {whale === undefined
+        ? <p>Please choose a whale.</p>
+        : <>
+            <h3>{whale.name}</h3>
+            <p>Life span: {whale.maxLifeSpan} yrs</p>
+            <p>Diet: {whale.diet} ({whale.favoriteFood})</p>
+            <p>Length: {whale.maxLengthInFt} ft</p>
+            <p>{whale.description}</p>
+            <img alt={whale.id} src={whale.imgSrc} />
+          </>
+      }
+    </>
+  )
+}
+
+function CurrentWhaleIdValue() {
+  const whaleId = useRecoilValue(currentWhaleIdState)
+
+  return (
+    <span>{whaleId.replace('_', ' ')}</span>
+  )
+}
+
+const App = () =>
+<RecoilRoot>
+  <Suspense fallback={<div>Loading whale types...</div>}>
+    <CurrentWhaleTypes />
+    <Suspense fallback={
+      <div>Loading <CurrentWhaleIdValue /> info...</div>
+    }>
+      <CurrentWhalePick />
+    }
+    </Suspense>
+  </Suspense>
+</RecoilRoot>
+
+export { App }
