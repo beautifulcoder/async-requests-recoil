8adbe6327321ef8e4fe0db3c591f84319a5454f2
Camilo Reyes <reyesc@comcast.net>
2023-08-16 00:34:05 -0500

    Upgrade react version to 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { reportWebVitals } from './reportWebVitals'
 
-render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
